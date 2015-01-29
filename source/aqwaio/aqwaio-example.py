beb78231eab86cd85d0f479724c5cdef353afd94
Michael Lawson <lawsonro3@gmail.com>
2015-01-29 14:04:07 -0700

    Update aqwaio-example.py

--- source/aqwaio/aqwaio-example.py
+++ source/aqwaio/aqwaio-example.py
@@ -10,8 +10,12 @@ import matplotlib.pyplot as plt
 plt.close('all')
 
 aq = aio.AqwaOutput('/Users/mlawson/Applications/python-scripts/source/aqwaio',outFile='aqwa-data.lis')
+
 aq.plotAddedMassAndDamping(body=0)
 aq.plotAddedMassAndDamping(body=1)
 aq.plotAddedMassAndDamping(body=2)
+
+aq.writeWecSimHydroData(bodyNumber=0)
 aq.writeWecSimHydroData(bodyNumber=1)
+aq.writeWecSimHydroData(bodyNumber=2)
 
