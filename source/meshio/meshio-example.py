920434dbb288359b027f4a910696bd4a5c9224a7
Michael Lawson <lawsonro3@gmail.com>
2015-03-31 11:23:17 -0600

    removing folders and files that were moved to bemio

--- source/meshio/meshio-example.py
+++
@@ -1,9 +0,0 @@
-import meshio as mio
-
-# Read mesh
-mesh = mio.readVtp('./NonSymmetrical.dat')
-
-# Write meshes
-#mesh.writeVtp('./NonSymmetrical-demoOutput.vtp')
-#mesh.writeNemohMesh('./NonSymmetrical-demoOutput.dat')
-#mesh.writeGdf('./NonSymmetrical-demoOutput.gdf')
