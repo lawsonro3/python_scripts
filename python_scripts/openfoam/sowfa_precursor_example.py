411eed8c85c24325f5b3e2d8e2573f0cd004617e
lawsonro3 <lawsonro3@gmail.com>
2018-11-13 00:26:27 -0700

    update

--- python_scripts/openfoam/sowfa_precursor_example.py
+++ python_scripts/openfoam/sowfa_precursor_example.py
@@ -11,7 +11,7 @@ plt.close('all')
 
 # Creat sowfa_precursor object and enter neccisary user inputs
 infPer_001m_5m =  sowfa_precursor.Sim(
-    dir='/Users/mlawson/Peregrine/windsim/wake_steering/stableABLRuns/infPer_0.001m_5m',
+    dir='/projects/windsim/mlawson/wake_steering/stableABLRuns/infPer_0.001m_5m',
     log='log.3.ABLSolver',
     time_dir='25000',
     avg_time=29000,
@@ -22,7 +22,7 @@ infPer_001m_5m =  sowfa_precursor.Sim(
 infPer_001m_5m.input['heights'] = np.array([0,1/2,1,3/2,2,3,4,5])*infPer_001m_5m.input['windHeight']
 
 # Make plots
-infPer_001m_5m.theta_w_avg_cell()
-infPer_001m_5m.Umean_avg_nonnormalized()
-infPer_001m_5m.Tmean_avg_nonnormalized()
-infPer_001m_5m.variances_avg_cell()
+#infPer_001m_5m.theta_w_avg_cell()
+#infPer_001m_5m.Umean_avg_nonnormalized()
+#infPer_001m_5m.Tmean_avg_nonnormalized()
+#infPer_001m_5m.variances_avg_cell()
